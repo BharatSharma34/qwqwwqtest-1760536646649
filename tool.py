e0eeb9955a59bcf4cbba532786e014b0021f9efc
Sanjeev <duck@gmail.com>
2025-10-15 19:59:21 +0530

    Updated tool code via Admin Panel

---
+++ tool.py
@@ -0,0 +1,11 @@
+
+def main(event_body):
+   print("changed code")
+    
+    return {
+        "success": True,
+        "message": "Hello from testTool2Oct15! with chnages",
+        "data": event_body
+    }
+
+# You can add helper functions below
